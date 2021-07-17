87ecb420613d12bb32a14b7a122a39e06955820d
hbyyy <qjaduddl94@gmail.com>
2021-07-17 19:35:56 +0900

    feat: write tests of to-do item table

--- functional_test.py
+++ functional_test.py
@@ -67,10 +67,8 @@ class NewVisitorTest(unittest.TestCase):
         # '1: Buy peacock feathers' must be in the to-do list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'new to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Test 2nd item', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -11,3 +11,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post(reverse('home'), {'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode('utf8'))
+        self.assertTemplateUsed(response, 'lists/home.html')
