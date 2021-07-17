40783df5376c80c7e0e5e36dc09c74a3f4f12851
hbyyy <qjaduddl94@gmail.com>
2021-07-17 19:45:53 +0900

    feat: table item checking test method implementation

--- functional_test.py
+++ functional_test.py
@@ -35,6 +35,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # user access the index page
         self.browser.get('http://localhost:8000')
@@ -63,6 +68,17 @@ class NewVisitorTest(unittest.TestCase):
         # when user hits enter, add to-do item, and page immediately update, and input_box text erase
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # 2nd to-do item input
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # check the table has 1st and 2nd items
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # '1: Buy peacock feathers' must be in the to-do list
         table = self.browser.find_element_by_id('id_list_table')
